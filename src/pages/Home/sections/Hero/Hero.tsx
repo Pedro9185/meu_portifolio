12079d6e826fad6138e0e71e757d6ba132fa78d9
Pedro9185 <pedrocatarina.ip@gmail.com>
2025-01-27 11:58:16 -0300

    feat: Created Hero components UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,34 +1,61 @@
 import DownloadIcon from "@mui/icons-material/Download";
 import EmailIcon from "@mui/icons-material/Email";
-import { Button, Container, Grid, styled, Typography } from "@mui/material";
+import { Box, Container, Grid, styled, Typography } from "@mui/material";
 import Avatar from "../../../../assets/images/avatar.jpg";
+import { AnimatedBackground } from "../../../../components/AnimatedBackround/AnimatedBackround";
+import StyledButton from "../../../../components/StyledButton/StyledButton";
 
 const Hero = () => {
-  const StyledHero = styled("div")(() => ({
-    backgroundColor: "black",
+  const StyledHero = styled("div")(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
     height: "100vh",
+    display: "flex",
+    alignItems: "center",
   }));
 
-  const StyledImg = styled("img")(() => ({
-    width: "100%",
+  const StyledImg = styled("img")(({ theme }) => ({
+    width: "70%",
     borderRadius: "50%",
+    border: `1px solid ${theme.palette.primary.contrastText}`,
   }));
   return (
     <>
       <StyledHero>
         <Container maxWidth={"lg"}>
           <Grid container spacing={2}>
-            <Grid item xs={12} md={4}>
-              <StyledImg src={Avatar} />
+            <Grid item xs={12} md={5}>
+              <Box position={"relative"}>
+                <Box position={"absolute"} width={"100%"} top={-100} right={0}>
+                  <AnimatedBackground />
+                </Box>
+                <Box position={"relative"} textAlign="center">
+                  <StyledImg src={Avatar} />
+                </Box>
+              </Box>
             </Grid>
-            <Grid item xs={12} md={8}>
-              <Typography color="primary" variant="h1" textAlign={"center"}>
+            <Grid item xs={12} md={7}>
+              <Typography
+                color="primary.contrastText"
+                variant="h1"
+                textAlign={"center"}
+              >
                 Pedro Henrique
               </Typography>
-              <Typography color="primary" variant="h2" textAlign={"center"}>
+              <Typography
+                color="primary.contrastText"
+                variant="h2"
+                textAlign={"center"}
+                pb={2}
+              >
                 I'm a Software Engineer
               </Typography>
-              <Grid container display={"flex"} justifyContent={"center"}>
+              <Grid
+                container
+                display={"flex"}
+                justifyContent={"center"}
+                spacing={3}
+                pt={3}
+              >
                 <Grid
                   item
                   xs={12}
@@ -36,10 +63,10 @@ const Hero = () => {
                   display={"flex"}
                   justifyContent={"center"}
                 >
-                  <Button>
+                  <StyledButton>
                     <DownloadIcon />
-                    Download CV
-                  </Button>
+                    <Typography>Download CV</Typography>
+                  </StyledButton>
                 </Grid>
                 <Grid
                   item
@@ -48,10 +75,10 @@ const Hero = () => {
                   display={"flex"}
                   justifyContent={"center"}
                 >
-                  <Button>
+                  <StyledButton>
                     <EmailIcon />
-                    Contact me
-                  </Button>
+                    <Typography>Contact me</Typography>
+                  </StyledButton>
                 </Grid>
               </Grid>
             </Grid>
--- src/theme.ts
+++ src/theme.ts
@@ -7,7 +7,7 @@ let theme = createTheme({
       main: '#232323',
     },
     secondary: {
-      main: '#4f8e3e',
+      main: '#C040FF',
     },
   },
   typography:{
